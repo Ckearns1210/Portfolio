013da555ff3695d7808c32ce78f6caaf0db877ea
Colin Kearns <ckearns1210@gmail.com>
2016-01-21 21:17:56 -0500

    uploaded

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function toggleOn(){
-  $('body, #projects-button, aside, section').toggleClass('on');
+function toggleOn() {
+  $('body, #projects-button, aside, section, .site-container').toggleClass('on');
 }
 
 
@@ -12,4 +12,21 @@ $(document).ready(function() {
     if ($('aside').hasClass('on')) {
       toggleOn();
     };
-  })});
+  })
+  $('body').keyup(function(e) {
+    if (e.keyCode == 32) {
+      toggleOn();
+    }
+  });
+  $('.glitch_word_box').on('touchstart', function(e) {
+    $('.glitch_word_box').addClass('hover');
+  }).on('touchmove', function(e) {
+    $('.glitch_word_box').removeClass('hover');
+  }).mouseenter(function(e) {
+    $('.glitch_word_box').addClass('hover');
+  }).mouseleave(function(e) {
+    $('.glitch_word_box').removeClass('hover');
+  }).click(function(e) {
+    $('.glitch_word_box').removeClass('hover');
+  });
+});
